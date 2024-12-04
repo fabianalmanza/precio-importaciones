e9a0e1d122933f4f6db2f52a370511ab2e183866
fabian <fabian.almanza741@gmail.com>
2024-12-04 11:17:51 -0500

    Updates

--- src/utils/formatters.js
+++
@@ -1,8 +0,0 @@
-export const formatNumber = (number, decimals = 2) => {
-  if (typeof number !== 'number' || isNaN(number)) return '0.00';
-  
-  return new Intl.NumberFormat('es-CO', {
-    minimumFractionDigits: decimals,
-    maximumFractionDigits: decimals,
-  }).format(number);
-};
